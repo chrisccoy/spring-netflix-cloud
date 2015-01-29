9ac66300968d8ec2829b56058bf6e732b68bd7c3
chrisccoy <chrisccoy@gmail.com>
2015-01-29 14:50:49 -0700

    adding new mods;

---
+++ server2/src/main/java/demo/HelloServerApplication2.java
@@ -0,0 +1,31 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Spencer Gibb
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class HelloServerApplication2 {
+	@Autowired
+	DiscoveryClient client;
+
+	@RequestMapping("/microsvc2/dosomethingelse")
+	public String hello2() {
+		ServiceInstance localInstance = client.getLocalServiceInstance();
+		return "Just did something else: "+ localInstance.getServiceId()+":"+localInstance.getHost()+":"+localInstance.getPort();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServerApplication2.class, args);
+	}
+}
